598bbc2667ced738794cb2e77c625e8b03f42f21
FrierenDv <hackerxct007@gmail.com>
2025-02-01 06:17:52 +0700

    fix: Comment out unused sdwebui-api-client execution in build script

--- scripts/build.js
+++ scripts/build.js
@@ -18,17 +18,17 @@ try {
 	// Post-compile
 	fs.copyFileSync("dist/index.d.ts", "dist/index.d.mts");
 
-	execSync("node -e 'require(\"@xct007/sdwebui-api-client\")'", {
-		cwd: "dist",
-		stdio: "inherit",
-	});
-	execSync(
-		"node -e 'import(\"@xct007/sdwebui-api-client\")' --input-type=module",
-		{
-			cwd: "dist",
-			stdio: "inherit",
-		}
-	);
+	// execSync("node -e 'require(\"@xct007/sdwebui-api-client\")'", {
+	// 	cwd: "dist",
+	// 	stdio: "inherit",
+	// });
+	// execSync(
+	// 	"node -e 'import(\"@xct007/sdwebui-api-client\")' --input-type=module",
+	// 	{
+	// 		cwd: "dist",
+	// 		stdio: "inherit",
+	// 	}
+	// );
 } catch (_error) {
 	process.exit(1);
 }
