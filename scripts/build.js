46ee29b2054678bd1bdc4fd46ba18a0e776ea5b5
FrierenDv <hackerxct007@gmail.com>
2025-02-01 06:06:51 +0700

    fix: Update build script to use sdwebui-api-client for post-compile execution

--- scripts/build.js
+++ scripts/build.js
@@ -18,14 +18,17 @@ try {
 	// Post-compile
 	fs.copyFileSync("dist/index.d.ts", "dist/index.d.mts");
 
-	execSync("node -e 'require(\"@xct007/sd-api\")'", {
+	execSync("node -e 'require(\"@xct007/sdwebui-api-client\")'", {
 		cwd: "dist",
 		stdio: "inherit",
 	});
-	execSync("node -e 'import(\"@xct007/sd-api\")' --input-type=module", {
-		cwd: "dist",
-		stdio: "inherit",
-	});
+	execSync(
+		"node -e 'import(\"@xct007/sdwebui-api-client\")' --input-type=module",
+		{
+			cwd: "dist",
+			stdio: "inherit",
+		}
+	);
 } catch (_error) {
 	process.exit(1);
 }
